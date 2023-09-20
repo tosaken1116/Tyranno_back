86f48efecdb13120e8caeb97de5f12a2c6beb2f8
PigeonsHouse <bullbull1230@gmail.com>
2023-09-21 00:26:18 +0900

    feat: ランダムにした

--- utils/generate_random_delay_time.go
+++ utils/generate_random_delay_time.go
@@ -1,8 +1,19 @@
 package utils
 
-import "time"
+import (
+	"crypto/rand"
+	"math/big"
+	"nnyd-back/config"
+	"time"
+)
 
 func GenerateRandomDelayTime() (delay_time time.Duration) {
-	// TODO: ここをランダムにする
-	return time.Duration(0)
+	duration := int64(config.POST_MAX_DELAY_TIME - config.POST_MIN_DELAY_TIME)
+
+	randomValue := 60
+	randomBigValue, err := rand.Int(rand.Reader, big.NewInt(duration))
+	if err == nil {
+		randomValue = int(randomBigValue.Int64())
+	}
+	return time.Duration(randomValue + 1 + config.POST_MIN_DELAY_TIME)
 }
